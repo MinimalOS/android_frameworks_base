04dbefcb20dc7b07c0a87d3dc63f39ab8bd3e7c2
Thiago Vinícius Freire de Araújo Ribeiro <thiagovfar@gmail.com>
2012-12-25 20:10:09 -0300

    [1/2] FW: Swipe to switch
    Adds the ability to switch to/from Quick Settings, using a sideways
    swipe gesture.
    You can turn on or off this feature in RC.
    Also includes this patch - http://review.cyanogenmod.org/#/c/30867/
    
    Video demonstration: http://www.youtube.com/watch?v=jZaDVBIFAuA
    (Video for the initial Patch Set only)
    
    Change-Id: I930673565f2535a0a0058e1d4ff8b5fccf74c251
    Signed-off-by: iceandfire <arhamjamal@gmail.com>
    
    Conflicts:
    	core/java/android/provider/Settings.java

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -2890,20 +2890,6 @@ public final class Settings {
         public static final String QUICK_TOGGLE_VIBRATE = "statusbar_toggles_vibrate_on_click";
 
         /**
-         * enable and disable fast toggle in settings
-         *
-         * @hide
-         */
-        public static final String FAST_TOGGLE = "fast_toggle";
-
-        /**
-         * enable and disable fast toggle in settings
-         *
-         * @hide
-         */
-        public static final String CHOOSE_FASTTOGGLE_SIDE = "choose_fasttoggle_side";
-
-        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *
@@ -7142,6 +7128,27 @@ public final class Settings {
          * @hide
          */
         public static final String QUICK_TOGGLE_VIBRATE = "statusbar_toggles_vibrate_on_click";
+
+        /**
+         * enable and disable fast toggle in settings
+         *
+         * @hide
+         */
+        public static final String FAST_TOGGLE = "fast_toggle";
+
+        /**
+         * enable and disable fast toggle in settings
+         *
+         * @hide
+         */
+        public static final String CHOOSE_FASTTOGGLE_SIDE = "choose_fasttoggle_side";
+
+        /**
+         * Swipe to switch from notifications to tiles
+         *
+         * @hide
+         */
+        public static final String SWIPE_TO_SWITCH = "swipe_to_switch";
 
     }
 
