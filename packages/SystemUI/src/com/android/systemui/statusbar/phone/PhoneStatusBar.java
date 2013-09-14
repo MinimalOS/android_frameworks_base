38e5da30a6f4b8c1ac210076162af36417e33f3d
0xD34D <clark@scheffsblend.com>
2013-09-14 13:12:09 -0700

    [1/3] Base: Squashed commits for 4.4 active display
    Add active display to ChameleonOS [1/2]
    Change-Id: I51c17cec33b65dc60111a10c5ca2c449596ebe63
    
    Correct active display settings naming
    Change-Id: Ibfa2d41eed01d8987df52f013b111a0ce4e70739
    
    Active display: Add options for date and am/pm [1/2]
    Change-Id: Idc09772829cc6aa9bcfb064459fb3557c7b37f6b
    
    Active display: Add sunlight mode. [1/2]
    Change-Id: I541e74c5a1a7e6ce5028ab00cf998bba1d4fe0f9
    
    Delay hiding systemui by 100ms
    Change-Id: Iad432f75195983837d643fa38495e6beb54f3e22
    
    Active display: make sure mNotificaiton is set correctly
    Change-Id: I4111ca843e340cca4d6dac4b2c2922ce3039e487
    
    Active display: register/unregister recievers and listeners when enabled/disabled
    Change-Id: Ic73e63da2c9634bc864ab1a1387eb9145eb0b5b0
    
    Active display: Unify (un)registering callbacks and add proper checks.
    Change-Id: Ie4c4660a54d2c60831f7a3bcc66b3a9ca1d035f2
    
    Active display: allow hiding low priority notifications. [1/2]
    Change-Id: I4831d6864b509e62b449c25619c59f5d6760ec47
    
    Active display: Enable proximity sensor only when screen is off
    Change-Id: I70979b5931815adb74bd8bc42e32886a6d829a12
    
    Active display: Use a default notificaiton icon.
    Change-Id: I4087ef3e407cf976f7bfb2bb89f850641101bc73
    
    Active display: Only fetch next notification if mNotification is null.
    Change-Id: Ife8511d4a4b8c448bc7aa8997a250068bec5fae7
    
    Active display: Don't show active display if call state is not idle.
    Change-Id: I2cb79e097ae24dc6ad6c8a33be88eb19f7d8d235
    
    Active display: Allow apps to be excluded from being displayed [1/2]
    Change-Id: I5c334833b78325733ae076c07afccebb02758ead
    
    4.4 implementation of active display
    
    Settings: https://gerrit.omnirom.org/#/c/2586/
    OmniGears: https://gerrit.omnirom.org/#/c/2584/
    Change-Id: I08330a28d0880f82c09ccaec7e36844d9e6570d5
    Signed-off-by: iceandfire <arhamjamal@gmail.com>
    
    Conflicts:
    	core/res/res/values/pa_strings.xml
    	packages/SystemUI/res/values/styles.xml
    	packages/SystemUI/src/com/android/systemui/statusbar/BaseStatusBar.java
    	packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
    
    Conflicts:
    	core/java/com/android/internal/widget/multiwaveview/GlowPadView.java
    	core/res/res/values/custom_arrays.xml
    	core/res/res/values/custom_strings.xml
    	packages/SystemUI/AndroidManifest.xml
    	packages/SystemUI/res/values/styles.xml
    	packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
@@ -86,6 +86,8 @@ import android.widget.ImageView;
 import android.widget.FrameLayout;
 import android.widget.LinearLayout;
 import android.widget.ScrollView;
+import android.widget.TabHost;
+import android.widget.TabHost.TabSpec;
 import android.widget.TextView;
 
 import com.android.internal.statusbar.StatusBarIcon;
@@ -483,6 +485,11 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode {
             }
         } catch (RemoteException ex) {
             // no window manager? good luck with that
+        }
+
+        if (mRecreating) {
+        } else {
+            addActiveDisplayView();
         }
 
         // figure out which pixel-format to use for the status bar.
@@ -2161,7 +2168,7 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode {
         mHandler.postDelayed(mAutohide, 350); // longer than app gesture -> flag clear
     }
 
-    private boolean areLightsOn() {
+    public boolean areLightsOn() {
         return 0 == (mSystemUiVisibility & View.SYSTEM_UI_FLAG_LOW_PROFILE);
     }
 
@@ -2181,6 +2188,7 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode {
         }
     }
 
+    @Override
     public void topAppWindowChanged(boolean showMenu) {
         if (DEBUG) {
             Log.d(TAG, (showMenu?"showing":"hiding") + " the MENU button");
