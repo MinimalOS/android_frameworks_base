0c7c21c71a9bb21ac9ca6d52b1c17ea82c7fd2e4
maxwen <max.weninger@gmail.com>
2013-11-26 03:16:11 +0100

    [1/3] base: battery light settings
    This also adds a new "level" for 100% full battery
    to get rid of the comments from people who complain
    that 90% is shown as full :)
    
    required overlay:
        <!-- Is the battery LED intrusive? Used to decide if there should be a disable option -->
        <bool name="config_intrusiveBatteryLed">true</bool>
    
        This will enable if the batter light option is shown in settings
    
        <!-- Does the battery LED support multiple colors? Used to decide if the user can change the colors -->
        <bool name="config_multiColorBatteryLed">true</bool>
    
        Also set this for devices like the HTC one which has only
        two colors. This will be handled in the color selction dialog.
        Dont set if for devices like flo which only has white led
    
    Change-Id: I6a173d19b4e1135174de73a9f528a61d81eb50da
    
    Conflicts:
    	core/java/android/provider/Settings.java
    	services/java/com/android/server/BatteryService.java

--- services/java/com/android/server/BatteryService.java
+++ services/java/com/android/server/BatteryService.java
@@ -139,6 +139,7 @@ public final class BatteryService extends Binder {
     private int mBatteryLowARGB;
     private int mBatteryMediumARGB;
     private int mBatteryFullARGB;
+    private int mBatteryReallyFullARGB;
     private boolean mMultiColorLed;
 
     private boolean mSentLowBatteryBroadcast = false;
@@ -770,8 +771,13 @@ public final class BatteryService extends Binder {
             } else if (status == BatteryManager.BATTERY_STATUS_CHARGING
                     || status == BatteryManager.BATTERY_STATUS_FULL) {
                 if (status == BatteryManager.BATTERY_STATUS_FULL || level >= 90) {
-                    // Battery is full or charging and nearly full
-                    mBatteryLight.setColor(mBatteryFullARGB);
+                    if (level == 100){
+                        // Battery is really full
+                        mBatteryLight.setColor(mBatteryReallyFullARGB);
+                    } else {
+                        // Battery is full or charging and nearly full
+                        mBatteryLight.setColor(mBatteryFullARGB);
+                    }
                 } else {
                     // Battery is charging and halfway full
                     mBatteryLight.setColor(mBatteryMediumARGB);
@@ -819,6 +825,9 @@ public final class BatteryService extends Binder {
                 resolver.registerContentObserver(Settings.System.getUriFor(
                         Settings.System.BATTERY_LIGHT_FULL_COLOR), false, this,
                     UserHandle.USER_ALL);
+                resolver.registerContentObserver(Settings.System.getUriFor(
+                        Settings.System.BATTERY_LIGHT_REALLY_FULL_COLOR), false, this,
+                    UserHandle.USER_ALL);
             }
 
             // Quiet Hours
@@ -859,19 +868,20 @@ public final class BatteryService extends Binder {
             // Light colors
             mBatteryLowARGB = Settings.System.getIntForUser(resolver,
                     Settings.System.BATTERY_LIGHT_LOW_COLOR,
-                    res.getInteger(
-                        com.android.internal.R.integer.config_notificationsBatteryLowARGB),
+                    mBatteryLowARGB,
                     UserHandle.USER_CURRENT_OR_SELF);
             mBatteryMediumARGB = Settings.System.getIntForUser(resolver,
                     Settings.System.BATTERY_LIGHT_MEDIUM_COLOR,
-                    res.getInteger(
-                        com.android.internal.R.integer.config_notificationsBatteryMediumARGB),
+                    mBatteryMediumARGB,
                     UserHandle.USER_CURRENT_OR_SELF);
             mBatteryFullARGB = Settings.System.getIntForUser(resolver,
                     Settings.System.BATTERY_LIGHT_FULL_COLOR,
-                    res.getInteger(
-                        com.android.internal.R.integer.config_notificationsBatteryFullARGB),
+                     mBatteryFullARGB,
                     UserHandle.USER_CURRENT_OR_SELF);
+            mBatteryReallyFullARGB = Settings.System.getIntForUser(resolver,
+                    Settings.System.BATTERY_LIGHT_REALLY_FULL_COLOR,
+                     mBatteryFullARGB,
+                     UserHandle.USER_CURRENT_OR_SELF);
 
             updateLedPulse();
         }
