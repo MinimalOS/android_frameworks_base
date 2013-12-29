4781d246416748cf3a8363b7b2accc7a70c46c49
iceandfire <arhamjamal@gmail.com>
2013-12-29 23:30:16 +0530

    FW: Incresing ring feature (1/2)
    Second part in packages/services/Telephony
    
    Credits: CM
    
    Signed-off-by: iceandfire <arhamjamal@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -2669,6 +2669,24 @@ public final class Settings {
         public static final String NON_INTRUSIVE_INCALL = "non_intrusive_incall";
 
         /**
+         * Whether the phone ringtone should be played in an increasing manner
+         * @hide
+         */
+        public static final String INCREASING_RING = "increasing_ring";
+
+        /**
+         * Minimum volume index for increasing ring volume
+         * @hide
+         */
+        public static final String INCREASING_RING_MIN_VOLUME = "increasing_ring_min_vol";
+
+        /**
+         * Time (in ms) between ringtone volume increases
+         * @hide
+         */
+        public static final String INCREASING_RING_INTERVAL = "increasing_ring_interval";
+
+        /**
          * Whether to use the custom quick unlock screen control
          * @hide
          */
