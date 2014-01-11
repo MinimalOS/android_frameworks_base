9dc58e59f41dc0cb56f6594a14c796ed6373ce29
Anu6is <curtis.mayers@gmail.com>
2014-01-10 20:19:01 -0600

    [1/2] Frameworks: Combine LTE & 2G/3G Tiles
    This combines both network tiles into one tile.
    2G -> 3G -> 2G3G -> LTE
    
    Linked commit: packages/services/Telephony
    
    Change-Id: Ia0d8edaec0e920b9ba174ec6ce49c5e7b832a5ab

--- telephony/java/android/telephony/TelephonyManager.java
+++ telephony/java/android/telephony/TelephonyManager.java
@@ -716,17 +716,6 @@ public class TelephonyManager {
     /**
      * {@hide}
      */
-    public void toggleLTE(boolean on) {
-        try {
-            getITelephony().toggleLTE(on);
-        } catch (RemoteException e) {
-            //Silently fail
-        }
-    }
-
-    /**
-     * {@hide}
-     */
     public void toggleMobileNetwork(int networkState) {
         try {
             getITelephony().toggleMobileNetwork(networkState);
