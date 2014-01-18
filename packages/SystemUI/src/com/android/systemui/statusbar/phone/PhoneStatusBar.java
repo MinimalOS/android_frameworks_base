30adfa79316502b40e47ac7227486ebeeef6001f
iceandfire <arhamjamal@gmail.com>
2014-01-18 12:36:45 +0530

    Revert "[1/3] Base: Squashed commits for 4.4 active display"
    This reverts commit 38e5da30a6f4b8c1ac210076162af36417e33f3d.
    
    Signed-off-by: iceandfire <arhamjamal@gmail.com>
    
    Conflicts:
    	packages/SystemUI/res/values/custom_dimens.xml
    	packages/SystemUI/res/values/custom_styles.xml
    	packages/SystemUI/src/com/android/systemui/statusbar/BaseStatusBar.java

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
@@ -86,8 +86,6 @@ import android.widget.ImageView;
 import android.widget.FrameLayout;
 import android.widget.LinearLayout;
 import android.widget.ScrollView;
-import android.widget.TabHost;
-import android.widget.TabHost.TabSpec;
 import android.widget.TextView;
 
 import com.android.internal.statusbar.StatusBarIcon;
@@ -485,11 +483,6 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode {
             }
         } catch (RemoteException ex) {
             // no window manager? good luck with that
-        }
-
-        if (mRecreating) {
-        } else {
-            addActiveDisplayView();
         }
 
         // figure out which pixel-format to use for the status bar.
@@ -2169,7 +2162,7 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode {
         mHandler.postDelayed(mAutohide, 350); // longer than app gesture -> flag clear
     }
 
-    public boolean areLightsOn() {
+    private boolean areLightsOn() {
         return 0 == (mSystemUiVisibility & View.SYSTEM_UI_FLAG_LOW_PROFILE);
     }
 
@@ -2189,7 +2182,6 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode {
         }
     }
 
-    @Override
     public void topAppWindowChanged(boolean showMenu) {
         if (DEBUG) {
             Log.d(TAG, (showMenu?"showing":"hiding") + " the MENU button");
