8238a95a4f57681d96976e66c85ebec0976faa0f
iceandfire <arhamjamal@gmail.com>
2014-01-19 18:40:32 +0530

    Revert "Revert "base: ActiveDisplay: fix for disappearing notification bar""
    This reverts commit 2d09951f2542653e51b7d4ebe24a91118100f6c5.

--- packages/SystemUI/src/com/android/systemui/statusbar/policy/activedisplay/ActiveDisplayView.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/policy/activedisplay/ActiveDisplayView.java
@@ -74,6 +74,7 @@ import android.widget.RemoteViews;
 import com.android.internal.widget.multiwaveview.GlowPadView;
 import com.android.internal.widget.multiwaveview.GlowPadView.OnTriggerListener;
 import com.android.internal.widget.multiwaveview.TargetDrawable;
+import com.android.internal.widget.LockPatternUtils;
 
 import com.android.systemui.R;
 import com.android.systemui.statusbar.BaseStatusBar;
@@ -646,6 +647,11 @@ public class ActiveDisplayView extends FrameLayout {
         setVisibility(View.GONE);
         restoreBrightness();
         mBar.disable(0);
+
+        // noone is taking care of reenable the bars in this case
+        if (isLockscreenDisabled()) {
+            mBar.disable(0);
+        }
         cancelTimeoutTimer();
         unregisterSensorListener(mLightSensor);
     }
@@ -1216,5 +1222,12 @@ public class ActiveDisplayView extends FrameLayout {
             return;
         String[] appsToExclude = excludedApps.split("\\|");
         mExcludedApps = new HashSet<String>(Arrays.asList(appsToExclude));
+    }
+
+    // copied from KeyguardViewMediator
+    private boolean isLockscreenDisabled() {
+        LockPatternUtils utils = new LockPatternUtils(mContext);
+        utils.setCurrentUser(UserHandle.USER_OWNER);
+        return utils.isLockScreenDisabled();
     }
 }
