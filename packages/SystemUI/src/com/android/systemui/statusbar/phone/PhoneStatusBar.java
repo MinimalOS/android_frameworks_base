caea74734b7e495bb09d3f4601df95d34a8cad62
BigBrother1984 <stevewatersy@gmail.com>
2014-02-03 19:25:45 +0100

    Quicksettings: Remove edit mode button flip animation.
    Make things less fancy.
    
    Signed-off-by: Carlo Savignano <stevewatersy@gmail.com>

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
@@ -244,7 +244,6 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode,
     View mDateTimeView;
     View mClearButton;
     ImageView mSettingsButton, mNotificationButton, mEditModeButton;
-    boolean mAnimatingEditModeButton;
 
     private int shortClick = 0;
     private int longClick = 1;
@@ -2816,15 +2815,7 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode,
     private View.OnClickListener mEditModeButtonListener = new View.OnClickListener() {
         public void onClick(View v) {
             final boolean enabled = mSettingsContainer.isEditModeEnabled();
-            if(mAnimatingEditModeButton) return;
-            mAnimatingEditModeButton = true;
-            mEditModeButton.animate().rotationYBy(180)
-                    .setListener(new AnimatorListenerAdapter() {
-                        public void onAnimationEnd(Animator animation) {
-                            mSettingsContainer.setEditModeEnabled(!enabled);
-                            mAnimatingEditModeButton = false;
-                        }
-                });
+            mSettingsContainer.setEditModeEnabled(!enabled);
         }
     };
 
