f081faa73e153d6b6b85de166a89cd96e5e0900f
Jorge Ruesga <jorge@ruesga.com>
2014-02-09 23:12:33 +0100

    core: add support for translucent volume panel
    Add support for display the volume panel in translucent mode
    
    Patchset 4: Make it depends to high-end graphics flag
    
    Change-Id: I4f21a6cc88282789072a0e71ae696b754a5e083e
    Signed-off-by: Jorge Ruesga <jorge@ruesga.com>
    
    Conflicts:
    	core/java/android/view/VolumePanel.java

--- core/java/android/view/VolumePanel.java
+++ core/java/android/view/VolumePanel.java
@@ -18,6 +18,11 @@ package android.view;
 
 import com.android.internal.R;
 
+import android.animation.Animator;
+import android.animation.Animator.AnimatorListener;
+import android.animation.AnimatorSet;
+import android.animation.ObjectAnimator;
+import android.app.ActivityManager;
 import android.app.AlertDialog;
 import android.app.Dialog;
 import android.content.DialogInterface.OnDismissListener;
@@ -42,6 +47,7 @@ import android.os.Vibrator;
 import android.provider.Settings;
 import android.util.Log;
 import android.view.WindowManager.LayoutParams;
+import android.view.animation.AccelerateInterpolator;
 import android.widget.ImageView;
 import android.widget.SeekBar;
 import android.widget.SeekBar.OnSeekBarChangeListener;
@@ -104,11 +110,18 @@ public class VolumePanel extends Handler implements OnSeekBarChangeListener, Vie
     private static final int STREAM_MASTER = -100;
     // Pseudo stream type for remote volume is defined in AudioService.STREAM_REMOTE_MUSIC
 
+    private static final int TRANSLUCENT_START_LEVEL = 160;
+    private static final int TRANSLUCENT_TO_OPAQUE_DURATION = 400;
+
     protected Context mContext;
     private AudioManager mAudioManager;
     protected AudioService mAudioService;
     private boolean mRingIsSilent;
     private boolean mShowCombinedVolumes;
+
+    private final boolean mTranslucentDialog;
+    private boolean mShouldRunDropTranslucentAnimation = false;
+    private boolean mRunningDropTranslucentAnimation = false;
 
     // True if we want to play tones on the system stream when the master stream is specified.
     private boolean mPlayMasterStreamTones;
@@ -281,6 +294,7 @@ public class VolumePanel extends Handler implements OnSeekBarChangeListener, Vie
         mContext = context;
         mAudioManager = (AudioManager) context.getSystemService(Context.AUDIO_SERVICE);
         mAudioService = volumeService;
+        mTranslucentDialog = ActivityManager.isHighEndGfx();
 
         LayoutInflater inflater = (LayoutInflater) context
                 .getSystemService(Context.LAYOUT_INFLATER_SERVICE);
@@ -375,6 +389,8 @@ public class VolumePanel extends Handler implements OnSeekBarChangeListener, Vie
         mPlayMasterStreamTones = masterVolumeOnly && masterVolumeKeySounds;
 
         listenToRingerMode();
+
+        applyTranslucentWindow();
     }
 
     public void setLayoutDirection(int layoutDirection) {
@@ -1016,6 +1032,7 @@ public class VolumePanel extends Handler implements OnSeekBarChangeListener, Vie
 
             case MSG_TIMEOUT: {
                 if (mDialog.isShowing()) {
+                    applyTranslucentWindow();
                     mDialog.dismiss();
                     mActiveStreamType = -1;
                 }
@@ -1075,6 +1092,9 @@ public class VolumePanel extends Handler implements OnSeekBarChangeListener, Vie
     }
 
     public void onStartTrackingTouch(SeekBar seekBar) {
+        if (mTranslucentDialog && mShouldRunDropTranslucentAnimation) {
+            startRemoveTranslucentAnimation();
+        }
     }
 
     public void onStopTrackingTouch(SeekBar seekBar) {
@@ -1092,9 +1112,53 @@ public class VolumePanel extends Handler implements OnSeekBarChangeListener, Vie
     }
 
     public void onClick(View v) {
-        if (v == mMoreButton) {
+        if (mTranslucentDialog && mShouldRunDropTranslucentAnimation) {
+            startRemoveTranslucentAnimation();
+        } else if (v == mMoreButton) {
             expand();
         }
         resetTimeout();
+    }
+
+    private void applyTranslucentWindow() {
+        if (!mTranslucentDialog || mRunningDropTranslucentAnimation) return;
+
+        mPanel.getBackground().setAlpha(TRANSLUCENT_START_LEVEL);
+        mMoreButton.setAlpha(0.0f);
+        mDivider.setAlpha(0.0f);
+        mShouldRunDropTranslucentAnimation = true;
+    }
+
+    private void startRemoveTranslucentAnimation() {
+        if (mRunningDropTranslucentAnimation) return;
+        mRunningDropTranslucentAnimation = true;
+
+        AnimatorSet set = new AnimatorSet();
+        Animator panelAlpha = ObjectAnimator.ofInt(
+                mPanel.getBackground(), "alpha", mPanel.getBackground().getAlpha(), 255);
+        Animator moreAlpha = ObjectAnimator.ofFloat(
+                mMoreButton, "alpha", mMoreButton.getAlpha(), 255);
+        Animator dividerAlpha = ObjectAnimator.ofFloat(
+                mDivider, "alpha", mDivider.getAlpha(), 255);
+        set.setInterpolator(new AccelerateInterpolator());
+        set.addListener(new AnimatorListener() {
+            @Override
+            public void onAnimationStart(Animator animation) {}
+
+            @Override
+            public void onAnimationRepeat(Animator animation) {}
+
+            @Override
+            public void onAnimationEnd(Animator animation) {
+                mRunningDropTranslucentAnimation = false;
+                mShouldRunDropTranslucentAnimation = false;
+            }
+
+            @Override
+            public void onAnimationCancel(Animator animation) {}
+        });
+        set.setDuration(TRANSLUCENT_TO_OPAQUE_DURATION);
+        set.playTogether(panelAlpha, moreAlpha, dividerAlpha);
+        set.start();
     }
 }
