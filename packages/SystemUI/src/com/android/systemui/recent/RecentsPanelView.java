949d8dc2f90d95e1877e0fb2231827beccc4675b
iceandfire <arhamjamal@gmail.com>
2014-02-17 00:14:30 +0530

    Long press 'Clear all' in recents panel to flush cache
    Long click to 'clear all' button in recents panel to force
    clear caches, dentries and inodes
    
    Change-Id: I3f1243beb1a2f75b8eafba90b88488f0bdb85025
    Signed-off-by: iceandfire <arhamjamal@gmail.com>

--- packages/SystemUI/src/com/android/systemui/recent/RecentsPanelView.java
+++ packages/SystemUI/src/com/android/systemui/recent/RecentsPanelView.java
@@ -80,6 +80,8 @@ import com.android.systemui.statusbar.phone.PhoneStatusBar;
 import com.android.internal.util.MemInfoReader;
 
 import java.util.ArrayList;
+import java.lang.Runtime;
+import java.io.OutputStreamWriter;
 
 public class RecentsPanelView extends FrameLayout implements OnItemClickListener, RecentsCallback,
         StatusBarPanel, Animator.AnimatorListener {
@@ -549,6 +551,23 @@ public class RecentsPanelView extends FrameLayout implements OnItemClickListener
                 @Override
                 public void onClick(View v) {
                     ((ViewGroup) mRecentsContainer).removeAllViewsInLayout();
+                }
+            });
+        mClearRecents.setOnLongClickListener(new OnLongClickListener() {
+                @Override
+                public boolean onLongClick(View v) {
+                    ((ViewGroup) mRecentsContainer).removeAllViewsInLayout();
+                    try {
+                        ProcessBuilder pb = new ProcessBuilder("su", "-c", "/system/bin/sh");
+                        OutputStreamWriter osw = new OutputStreamWriter(pb.start().getOutputStream());
+                        osw.write("sync" + "\n" + "echo 3 > /proc/sys/vm/drop_caches" + "\n");
+                        osw.write("\nexit\n");
+                        osw.flush();
+                        osw.close();
+                    } catch (Exception e) {
+                        Log.d(TAG, "Flush caches failed!");
+                    }
+                    return true;
                 }
             });
         }
