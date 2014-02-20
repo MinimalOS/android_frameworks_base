29f7ece3122ac9bc9c02a2e5004302caaff90d25
Quinten Johnson <joh07467@gmail.com>
2014-02-20 10:19:07 -0600

    [2/2] SystemUI: Add percent only and none options for battery icon
    Part 1: https://gerrit.omnirom.org/#/c/5722/
    JIRA: http://jira.omnirom.org/browse/OMNI-574
    
    Change-Id: I3c9c3820389d5073b1fa901cd2008bc685840a63
    
    Conflicts:
    	packages/SystemUI/res/layout/quick_settings_tile_battery.xml
    	packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
    	packages/SystemUI/src/com/android/systemui/statusbar/phone/QuickSettings.java
    	packages/SystemUI/src/com/android/systemui/statusbar/phone/QuickSettingsBasicBatteryTile.java

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
@@ -96,6 +96,7 @@ import android.widget.TextView;
 import com.android.internal.statusbar.StatusBarIcon;
 import com.android.systemui.BatteryMeterView;
 import com.android.systemui.BatteryCircleMeterView;
+import com.android.systemui.BatteryPercentMeterView;
 import com.android.systemui.DemoMode;
 import com.android.systemui.EventLogTags;
 import com.android.systemui.R;
@@ -216,6 +217,7 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode,
 
     private BatteryMeterView mBattery;
     private BatteryCircleMeterView mCircleBattery;
+    private BatteryPercentMeterView mPercentBattery;
 
     // expanded notifications
     NotificationPanelView mNotificationPanel; // the sliding/resizing panel within the notification window
@@ -378,9 +380,10 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode,
     };
 
     private void updateBatteryIcons() {
-        if (mBattery != null && mCircleBattery != null) {
+        if (mBattery != null && mCircleBattery != null && mPercentBattery != null) {
             mBattery.updateSettings();
             mCircleBattery.updateSettings();
+            mPercentBattery.updateSettings();
         }
     }
 
@@ -764,6 +767,7 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode,
         mNetworkController.setListener(this);
         mBattery = (BatteryMeterView) mStatusBarView.findViewById(R.id.battery);
         mCircleBattery = (BatteryCircleMeterView) mStatusBarView.findViewById(R.id.circle_battery);
+        mPercentBattery = (BatteryPercentMeterView) mStatusBarView.findViewById(R.id.percent_battery);
 
         return mStatusBarView;
     }
