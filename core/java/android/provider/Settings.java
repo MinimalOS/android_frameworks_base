c9d25968cc3a9ddb9ac7f73dbcc566a361851f33
GwonHyeok <kh4975@naver.com>
2014-03-03 11:43:07 +0000

    FW_Base - XuiMod: Toast Animations [1/2]
    Change-Id: Ic4d1b573b8ed5c657e8e9b226c28944d6a0895ec
    Signed-off-by: iceandfire <arhamjamal@gmail.com>
    
    Conflicts:
    	core/java/android/provider/Settings.java
    
    Conflicts:
    	core/res/res/values/xenonhd_symbols.xml

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -3578,6 +3578,12 @@ public final class Settings {
         public static final String T9_SEARCH_INPUT_LOCALE = "t9_search_input_locale";
 
         /**
+        * Toast animations
+        * @hide
+        */
+        public static final String TOAST_ANIMATION = "toast_animation";
+
+        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *
