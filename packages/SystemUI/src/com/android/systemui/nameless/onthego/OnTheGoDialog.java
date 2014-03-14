56108125af3584fba91041d355e5a7e8c993ef16
Evisceration <eviscerationls@gmail.com>
2014-03-15 00:07:28 +0100

    Base: On-The-Go: Change the way of changing the camera mode
    changing it on the fly is too buggy like i have done it.
    sometimes things fail and we are getting stuck ;P
    
    on mode change, save the value, create a notification and stop the service.
    the notification allows to restart the service.
    
    Change-Id: Ie02f01a4ab2f856cd58fe5fde8c49220bf7efd4b
    
    Conflicts:
    	packages/SystemUI/src/com/android/systemui/nameless/onthego/OnTheGoService.java

--- packages/SystemUI/src/com/android/systemui/nameless/onthego/OnTheGoDialog.java
+++ packages/SystemUI/src/com/android/systemui/nameless/onthego/OnTheGoDialog.java
@@ -74,7 +74,7 @@ public class OnTheGoDialog extends Dialog {
         final ContentResolver resolver = mContext.getContentResolver();
 
         final SeekBar mSlider = (SeekBar) findViewById(R.id.alpha_slider);
-        final float value = Settings.Nameless.getFloat(resolver,
+        final float value = Settings.System.getFloat(resolver,
                 Settings.System.ON_THE_GO_ALPHA,
                 0.5f);
         final int progress = ((int) (value * 100));
@@ -97,17 +97,17 @@ public class OnTheGoDialog extends Dialog {
         });
 
         final Switch mCamSwitch = (Switch) findViewById(R.id.onthego_camera_toggle);
-        final boolean useFrontCam = (Settings.Nameless.getInt(resolver,
-                Settings.Nameless.ON_THE_GO_CAMERA,
+        final boolean useFrontCam = (Settings.System.getInt(resolver,
+                Settings.System.ON_THE_GO_CAMERA,
                 0) == 1);
         mCamSwitch.setChecked(useFrontCam);
         mCamSwitch.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
             @Override
             public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
-                Settings.Nameless.putInt(resolver,
-                        Settings.Nameless.ON_THE_GO_CAMERA,
+                Settings.System.putInt(resolver,
+                        Settings.System.ON_THE_GO_CAMERA,
                         (b ? 1 : 0));
-                dismissOnTheGoDialog(mOnTheGoDialogShortTimeout);
+                mHandler.post(mDismissDialogRunnable);
             }
         });
     }
