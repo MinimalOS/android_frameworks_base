86e7e14df339014e1881d4883d5326ef6887656a
cristianomatos <cristianobmatos@gmail.com>
2014-03-25 14:43:13 -0300

    [base] long press back to kill configurable timeout (1/2) 1 - 2 seconds (default) 2 - 1,5 seconds 3 - 1 second 4 0,5 second
    Ported to lollipop by @iceandfire
    
    All credits to crdroid and cristianomatos
    
    Change-Id: I04e12877d60b78fbcd29dcf9cb8a6159ef2debd4
    Signed-off-by: iceandfire <arhamjamal@gmail.com>

--- policy/src/com/android/internal/policy/impl/PhoneWindowManager.java
+++ policy/src/com/android/internal/policy/impl/PhoneWindowManager.java
@@ -1604,8 +1604,6 @@ public class PhoneWindowManager implements WindowManagerPolicy {
                 com.android.internal.R.integer.config_deviceHardwareKeys);
         mHasRemovableLid = mContext.getResources().getBoolean(
                 com.android.internal.R.bool.config_hasRemovableLid);
-        mBackKillTimeout = mContext.getResources().getInteger(
-                com.android.internal.R.integer.config_backKillTimeout);
 
         updateKeyAssignments();
 
@@ -3334,6 +3332,8 @@ public class PhoneWindowManager implements WindowManagerPolicy {
             if (unpinActivity(true) || Settings.Secure.getInt(mContext.getContentResolver(),
                     Settings.Secure.KILL_APP_LONGPRESS_BACK, 0) == 1) {
                 if (down && repeatCount == 0) {
+                    mBackKillTimeout = Settings.Secure.getIntForUser(mContext.getContentResolver(),
+                        Settings.Secure.KILL_APP_LONGPRESS_TIMEOUT, 0, UserHandle.USER_CURRENT);
                     mHandler.postDelayed(mBackLongPress, mBackKillTimeout);
                 }
             }
