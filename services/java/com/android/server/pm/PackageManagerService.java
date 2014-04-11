ab139c9b8df30a30e870eda0163bfffc417e8ad2
nuclearmistake <nuclearmistake@gmail.com>
2014-04-11 19:04:15 -0400

    Show package names during dexopt
    ps2: Attempt to determine localized package label, and print that.
         If unavailable, print packageName
    
    Change-Id: If9ca78fe312acd674bdcee160f914b4445d5c85f

--- services/java/com/android/server/pm/PackageManagerService.java
+++ services/java/com/android/server/pm/PackageManagerService.java
@@ -3923,8 +3923,18 @@ public class PackageManagerService extends IPackageManager.Stub {
             final int pkgsSize = pkgs.size();
             ExecutorService executorService = Executors.newFixedThreadPool(sNThreads);
             final long start = System.currentTimeMillis();
+            PackageManager pm = null;;
+            if (mContext != null)
+                pm = mContext.getPackageManager();
+            String n = null;
             for (PackageParser.Package pkg : pkgs) {
                 final PackageParser.Package p = pkg;
+                if (pm != null)
+                    n = (String)p.applicationInfo.loadLabel(pm);
+                if (n == null || n.length() == 0)
+                    n = p.packageName;
+                final String name = "\n"+n;
+                n = null;
                 synchronized (mInstallLock) {
                     if (!p.mDidDexOpt) {
                         executorService.submit(new Runnable() {
@@ -3932,7 +3942,7 @@ public class PackageManagerService extends IPackageManager.Stub {
                             public void run() {
                                 if (!isFirstBoot()) {
                                     i[0]++;
-                                    postBootMessageUpdate(i[0], pkgsSize);
+                                    postBootMessageUpdate(i[0], pkgsSize, name);
                                 }
                                 performDexOptLI(p, false, false, true);
                             }
@@ -3940,7 +3950,7 @@ public class PackageManagerService extends IPackageManager.Stub {
                     } else {
                         if (!isFirstBoot()) {
                             i[0]++;
-                            postBootMessageUpdate(i[0], pkgsSize);
+                            postBootMessageUpdate(i[0], pkgsSize, name);
                         }
                     }
                 }
@@ -3956,12 +3966,12 @@ public class PackageManagerService extends IPackageManager.Stub {
         }
     }
 
-    private void postBootMessageUpdate(int n, int total) {
+    private void postBootMessageUpdate(int n, int total, final String name) {
         try {
             ActivityManagerNative.getDefault().showBootMessage(
                     mContext.getResources().getString(
                             com.android.internal.R.string.android_upgrading_apk,
-                            n, total), true);
+                            n, total, name), true);
         } catch (RemoteException e) {
         }
     }
