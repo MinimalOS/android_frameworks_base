f5394e4189d2f35bef3c2e6898dfa16af2d19608
linus_lee <llee@cyngn.com>
2015-02-05 15:47:37 -0800

    Enable Buttons and Status Bar settings Multi-user enabled
    The main issue is SystemUI process is shared across users, so calling getString(...)
    will call UserHandle.myUserId() which will return the user id of the process.
    To fix this, all relevant calls should be changed to get*ForUser as well as listen
    to UserChanged event so that when the user switches, the settings are updated
    
    Change-Id: I9ac3fbfec1646993028c34789a4299d85079f8f1
    Signed-off-by: iceandfire <arhamjamal@gmail.com>
    
    Conflicts:
    	core/java/android/provider/Settings.java
    	packages/SettingsProvider/src/com/android/providers/settings/DatabaseHelper.java
    	packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
    	packages/SystemUI/src/com/android/systemui/statusbar/policy/Clock.java

--- packages/SettingsProvider/src/com/android/providers/settings/DatabaseHelper.java
+++ packages/SettingsProvider/src/com/android/providers/settings/DatabaseHelper.java
@@ -74,7 +74,7 @@ public class DatabaseHelper extends SQLiteOpenHelper {
     // database gets upgraded properly. At a minimum, please confirm that 'upgradeVersion'
     // is properly propagated through your change.  Not doing so will result in a loss of user
     // settings.
-    private static final int DATABASE_VERSION = 118;
+    private static final int DATABASE_VERSION = 119;
 
     private static final String HEADSET = "_headset";
     private static final String SPEAKER = "_speaker";
@@ -1876,6 +1876,25 @@ public class DatabaseHelper extends SQLiteOpenHelper {
             moveSettingsToNewTable(db, TABLE_SYSTEM, TABLE_SECURE,
                     settingsToMove, true);
             upgradeVersion = 118;
+        }
+
+        if (upgradeVersion < 119) {
+            // only the owner has access to global table, so we need to check that here
+            if (mUserHandle == UserHandle.USER_OWNER) {
+                String[] globalToSecure = new String[] { Settings.Secure.POWER_MENU_ACTIONS };
+
+                moveSettingsToNewTable(db, TABLE_GLOBAL, TABLE_SECURE, globalToSecure, true);
+            }
+
+            String[] systemToSecure = new String[] {
+                    Secure.DEV_FORCE_SHOW_NAVBAR,
+                    Secure.KEYBOARD_BRIGHTNESS,
+                    Secure.BUTTON_BRIGHTNESS,
+                    Secure.BUTTON_BACKLIGHT_TIMEOUT
+            };
+            moveSettingsToNewTable(db, TABLE_SYSTEM, TABLE_SECURE, systemToSecure, true);
+
+            upgradeVersion = 119;
         }
 
         // *** Remember to update DATABASE_VERSION above!
