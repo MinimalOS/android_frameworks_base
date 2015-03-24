eca76b3474d6a955b52727d113da6be3dfb2a700
martincz <martincz@mokeedev.com>
2015-03-24 19:56:00 +0100

    Show carrier label / custom & change color [1/2]
    AICPfy:
    No DuUtils
    PS2: fix build
    PS3: lower font size to 14dp
    PS4: Use Statusbar clock style instead
    
    Change-Id: Ida3321c9176e4e786fad96766dab056d8614369d

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -3492,6 +3492,24 @@ public final class Settings {
         public static final String STATUS_BAR_MSIM_SHOW_EMPTY_ICONS = "status_bar_show_empty_sims";
 
         /**
+         * show carrier in statusbar. The value is
+         * int (1: Show or 0: Hide).
+         */
+        public static final String STATUS_BAR_CARRIER = "status_bar_carrier";
+
+        /**
+         * custom carrier label. The value is
+         * String.
+         */
+        public static final String CUSTOM_CARRIER_LABEL = "custom_carrier_label";
+
+        /**
+         * Carrier Label Custom Color
+         * @hide
+         */
+        public static final String STATUS_BAR_CARRIER_COLOR = "status_bar_carrier_color";
+
+        /**
          * Enable statusbar double tap gesture on to put device to sleep
          * @hide
          */
