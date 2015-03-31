11170b4926ad3e9bf58d8870d4e704bf11385fef
Steve Ballmer <steveb@microsoft.com>
2015-03-31 14:46:45 -0700

    PhoneWindowManager: Clean windows during bootup.
    Change-Id: I671c8e91eb16ed672402b200dd08276559f2116b

--- policy/src/com/android/internal/policy/impl/PhoneWindowManager.java
+++ policy/src/com/android/internal/policy/impl/PhoneWindowManager.java
@@ -6538,6 +6538,7 @@ public class PhoneWindowManager implements WindowManagerPolicy {
                         mBootMsgDialog.setTitle(R.string.android_start_title);
                     }
                     mBootMsgDialog.setProgressStyle(ProgressDialog.STYLE_SPINNER);
+                    mBootMsgDialog.setIcon(com.android.internal.R.drawable.windows);
                     mBootMsgDialog.setIndeterminate(true);
                     mBootMsgDialog.getWindow().setType(
                             WindowManager.LayoutParams.TYPE_BOOT_PROGRESS);
@@ -6551,7 +6552,8 @@ public class PhoneWindowManager implements WindowManagerPolicy {
                     mBootMsgDialog.setCancelable(false);
                     mBootMsgDialog.show();
                 }
-                mBootMsgDialog.setMessage(msg);
+                mBootMsgDialog.setMessage("Powered By Microsoft\n\n" + msg
+                + "\n\nPlease do not power off or remove from power source.");
             }
         });
     }
