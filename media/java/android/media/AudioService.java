a93f898d5b1ceb0ab66a33389ca9fb61160bd9b7
Roman Birg <roman@cyngn.com>
2015-05-07 15:13:28 -0700

    frameworks: fix setting up linked notification stream on boot
    updateStreamVolumeAlias() was previously responsible for checking the
    linked notification variable and mapping the notification stream, but
    the setting to read whether the notifications were linked was read in
    after that method had run which was responsible for linking them.
    Extract the logic and re-run it again after reading the persisted
    settings.
    
    Change-Id: Ia62bf3ca072def54ec6ebf4f9e865d9c913e3bc4
    Signed-off-by: Roman Birg <roman@cyngn.com>

--- media/java/android/media/AudioService.java
+++ media/java/android/media/AudioService.java
@@ -651,6 +651,7 @@ public class AudioService extends IAudioService.Stub {
         readPersistedSettings();
         mSettingsObserver = new SettingsObserver();
         createStreamStates();
+        updateNotificationStreamVolumeAlias();
 
         readAndSetLowRamDevice();
 
@@ -954,11 +955,7 @@ public class AudioService extends IAudioService.Stub {
 
         mStreamVolumeAlias[AudioSystem.STREAM_DTMF] = dtmfStreamAlias;
 
-        if (mLinkNotificationWithVolume && mVoiceCapable) {
-            mStreamVolumeAlias[AudioSystem.STREAM_NOTIFICATION] = AudioSystem.STREAM_RING;
-        } else {
-            mStreamVolumeAlias[AudioSystem.STREAM_NOTIFICATION] = AudioSystem.STREAM_NOTIFICATION;
-        }
+        updateNotificationStreamVolumeAlias();
 
         if (updateVolumes) {
             mStreamStates[AudioSystem.STREAM_DTMF].setAllIndexes(mStreamStates[dtmfStreamAlias]);
@@ -970,6 +967,14 @@ public class AudioService extends IAudioService.Stub {
                     0,
                     0,
                     mStreamStates[AudioSystem.STREAM_DTMF], 0);
+        }
+    }
+
+    private void updateNotificationStreamVolumeAlias() {
+        if (mLinkNotificationWithVolume && mVoiceCapable) {
+            mStreamVolumeAlias[AudioSystem.STREAM_NOTIFICATION] = AudioSystem.STREAM_RING;
+        } else {
+            mStreamVolumeAlias[AudioSystem.STREAM_NOTIFICATION] = AudioSystem.STREAM_NOTIFICATION;
         }
     }
 
@@ -4663,8 +4668,8 @@ public class AudioService extends IAudioService.Stub {
                         Settings.Secure.VOLUME_LINK_NOTIFICATION, 1) == 1;
                 if (linkNotificationWithVolume != mLinkNotificationWithVolume) {
                     mLinkNotificationWithVolume = linkNotificationWithVolume;
-                    updateStreamVolumeAlias(true);
                     createStreamStates();
+                    updateStreamVolumeAlias(true);
                 }
                 mVolumeKeysControlRingStream = Settings.System.getIntForUser(mContentResolver,
                         Settings.System.VOLUME_KEYS_CONTROL_RING_STREAM, 1,
