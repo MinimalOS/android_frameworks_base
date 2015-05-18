6431e8967cf59699c8814cd4ae737e10f28d44ac
CallMeAldy <aldrin.holmes20@gmail.com>
2015-05-18 11:56:26 +0200

    Revert "FWB: Network traffic meter (1/2)"
    This reverts commit c569097708c69a8fd24a54432c24cfce5684452d.
    
    Conflicts:
    	core/java/android/provider/Settings.java
    	packages/SystemUI/res/values/xenonhd_dimens.xml
    	packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBarTransitions.java

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -3510,16 +3510,6 @@ public final class Settings {
          */
         public static final String DOUBLE_TAP_SLEEP_NAVBAR = "double_tap_sleep_navbar";
 
-       /**
-         * Network traffic indicator, goes from least to greatest significant bitwise
-         * 0 = Display up-stream traffic if set
-         * 1 = Display down-stream traffic if set
-         * 2 = Show as Byte/s if set
-         * 16-31 = Refresh interval(ms) min: 250 max: 32750 default: 1000
-         * @hide
-         */
-        public static final String NETWORK_TRAFFIC_STATE = "network_traffic_state";
-
         /**
          * Boolean value on whether to show weather in the statusbar
          * @hide
