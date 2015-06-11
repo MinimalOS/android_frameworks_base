1efeb44324d7ecc7c2af7eb78b9f3dde67969ae6
cristianomatos <cristianobmatos@gmail.com>
2015-06-11 10:03:34 -0300

    Heads up snooze tweaks
    Heads up has a snooze function in 5.1.1 but a few know about it. If you swipe up from heads up it will dismiss the notification in dimissed package and heads up will not show for 1 minute. The same happens if you try to expand the status bar from a heads up notification (not a smart behavior).
    Now a toast message will be showed to inform the user and heads up will only be dismissed in swipe up action
    
    Change-Id: I21d5920888393c11079b7602ab87fa728a659b61
    
    Conflicts:
    	packages/SystemUI/res/values/cr_strings.xml

--- packages/SystemUI/src/com/android/systemui/statusbar/policy/HeadsUpNotificationView.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/policy/HeadsUpNotificationView.java
@@ -35,6 +35,7 @@ import android.view.ViewOutlineProvider;
 import android.view.ViewTreeObserver;
 import android.view.accessibility.AccessibilityEvent;
 import android.widget.FrameLayout;
+import android.widget.Toast;
 
 import com.android.systemui.ExpandHelper;
 import com.android.systemui.Gefingerpoken;
@@ -217,6 +218,9 @@ public class HeadsUpNotificationView extends FrameLayout implements SwipeHelper.
         if (mMostRecentPackageName != null) {
             mSnoozedPackages.put(snoozeKey(mMostRecentPackageName, mUser),
                     SystemClock.elapsedRealtime() + mSnoozeLengthMs);
+            Toast.makeText(mContext,
+                    mContext.getString(R.string.heads_up_snooze_message,
+                    mSnoozeLengthMs / 60 / 1000), Toast.LENGTH_LONG).show();
         }
         releaseAndClose();
     }
@@ -501,10 +505,12 @@ public class HeadsUpNotificationView extends FrameLayout implements SwipeHelper.
                     final float daX = Math.abs(ev.getX() - mFirstX);
                     final float daY = Math.abs(dY);
                     if (!mConsuming && daX < daY && daY > mTouchSlop) {
-                        snooze();
                         if (dY > 0) {
                             if (DEBUG_EDGE_SWIPE) Log.d(TAG, "found an open");
                             mBar.animateExpandNotificationsPanel();
+                            releaseAndClose();
+                        } else if (dY < 0) {
+                            snooze();
                         }
                         mConsuming = true;
                     }
