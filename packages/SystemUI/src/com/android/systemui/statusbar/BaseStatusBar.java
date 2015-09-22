3cbb88cc4d3720688f73ded63717ba26b0034abb
Aldrin Holmes (CallMeAldy) <aldrin.holmes20@gmail.com>
2015-09-22 14:07:53 +0200

    Revert "SystemUi: Prioritize call notifications which interrupt."
    This reverts commit 49eb87d92d235de1caa73db0990c3408f07a46b7.

--- packages/SystemUI/src/com/android/systemui/statusbar/BaseStatusBar.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/BaseStatusBar.java
@@ -484,7 +484,7 @@ public abstract class BaseStatusBar extends SystemUI implements
         public void onNotificationPosted(final StatusBarNotification sbn,
                 final RankingMap rankingMap) {
             if (DEBUG) Log.d(TAG, "onNotificationPosted: " + sbn);
-            final Runnable notificationPost = new Runnable() {
+            mHandler.post(new Runnable() {
                 @Override
                 public void run() {
                     Notification n = sbn.getNotification();
@@ -514,12 +514,7 @@ public abstract class BaseStatusBar extends SystemUI implements
                         addNotification(sbn, rankingMap);
                     }
                 }
-            };
-            if (TextUtils.equals(sbn.getNotification().category, Notification.CATEGORY_CALL)) {
-                mHandler.postAtFrontOfQueue(notificationPost);
-            } else {
-                mHandler.post(notificationPost);
-            }
+            });
         }
 
         @Override
@@ -1240,7 +1235,7 @@ public abstract class BaseStatusBar extends SystemUI implements
     public abstract void resetHeadsUpDecayTimer();
     public abstract void hideHeadsUp();
 
-    public abstract void scheduleHeadsUpOpen(boolean immediate);
+    public abstract void scheduleHeadsUpOpen();
 
     public abstract void scheduleHeadsUpClose();
 
--- packages/SystemUI/src/com/android/systemui/statusbar/tv/TvStatusBar.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/tv/TvStatusBar.java
@@ -148,7 +148,7 @@ public class TvStatusBar extends BaseStatusBar {
     }
 
     @Override
-    public void scheduleHeadsUpOpen(boolean immediate) {
+    public void scheduleHeadsUpOpen() {
     }
 
     @Override
